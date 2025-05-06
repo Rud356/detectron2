a93df085b11f9ad600ac054798f9be6601fb94bf
Rud356 <r.moroshkin2015@gmail.com>
2025-05-06 23:47:04 +0300

    Fix deprecation warning

--- detectron2/model_zoo/model_zoo.py
+++ detectron2/model_zoo/model_zoo.py
@@ -1,7 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
+import importlib.resources
 import os
 from typing import Optional
-import pkg_resources
 import torch
 
 from detectron2.checkpoint import DetectionCheckpointer
@@ -136,9 +136,8 @@ def get_config_file(config_path):
     Returns:
         str: the real path to the config file.
     """
-    cfg_file = pkg_resources.resource_filename(
-        "detectron2.model_zoo", os.path.join("configs", config_path)
-    )
+    cfg_file = importlib.resources.files("detectron2.model_zoo").joinpath("configs", config_path)
+
     if not os.path.exists(cfg_file):
         raise RuntimeError("{} not available in Model Zoo!".format(config_path))
     return cfg_file
